dd752ad1002b83a66dcdf0f235fb55c9d2f5c7a4
devcxl <64475363+devcxl@users.noreply.github.com>
2026-05-04 18:52:47 +0800

    fix(data): bump Room database version to 2 to match @ColumnInfo schema changes

--- app/src/main/java/cn/devcxl/photosync/data/AppDatabase.java
+++ app/src/main/java/cn/devcxl/photosync/data/AppDatabase.java
@@ -11,7 +11,7 @@ import cn.devcxl.photosync.data.entity.SyncDevice;
 /**
  * @author devcxl
  */
-@Database(entities = {SyncDevice.class, PhotoEntity.class}, version = 1, exportSchema = false)
+@Database(entities = {SyncDevice.class, PhotoEntity.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static volatile AppDatabase INSTANCE;
 
